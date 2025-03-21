ae34eaca4ba12c2c821b03be01d36db5bf656e91
GM-11 <mathuyrgopal@gmail.com>
2025-03-22 02:10:58 +0530

    feat: misc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,50 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
 
 // Import your global CSS file
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#ffffff",
+          borderTopColor: "#e5e7eb",
+          paddingTop: 5,
+          height: 60,
+          elevation: 0,
+          shadowOpacity: 0.1,
+        },
+        tabBarActiveTintColor: "#e11d48", // rose-600
+        tabBarInactiveTintColor: "#9ca3af", // gray-400
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "500",
+          marginBottom: 5,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="create-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="gallery"
+        options={{
+          title: "Gallery",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="images-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
